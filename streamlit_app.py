97bb4e49a5d78c2820badd2bd1f03222b1726821
Martin Repa <105520048+MartinRepa@users.noreply.github.com>
2022-05-13 22:37:10 +0200

    Update streamlit_app.py
    Updated 'Breakfast Menu'

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,9 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
+
+#Breakfast Menu
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+#Breakfast Menu
