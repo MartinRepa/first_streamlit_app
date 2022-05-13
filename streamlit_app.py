2f44259933225bfedd4e27bf9d9aa5f4ebde6e1b
Martin Repa <105520048+MartinRepa@users.noreply.github.com>
2022-05-13 22:41:44 +0200

    Update streamlit_app.py
    Added another header - Build fruit smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,10 @@ streamlit.title('My Parents New Healthy Dinner')
 
 #Breakfast Menu
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach and Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 #Breakfast Menu
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
